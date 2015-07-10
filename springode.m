654c56c005b9fe28ea7c215e5c154a882c074607
prashanthr05 <prashanthramadoss@gmail.com>
2015-07-10 19:26:04 +0200

    added noisy measurement

--- springode.m
+++ springode.m
@@ -3,17 +3,28 @@
 close all;
 param.m = 1;
 param.k = 10;
-param.omega = 2; % if omega is a non-zero value, it's a forced system
+param.omega = 0; % if omega is a non-zero value, it's a forced system
 
 springmass = @(t,z)springprocess(z,t,param);
 
 tspan = [0 10];
 z0 = [0 1];
 
+%used as a process model for the EKF
 [t,z] = ode45(springmass,tspan,z0);
+
+%Obtaining a noisy velocity measurement of known variance for the measurement model
+variance = 1;
+wn = sqrt(variance).*randn(size(z(:,2)));
+y = z(:,2) + wn;
+
+
 figure(1)
 plot(t,z(:,1));
 title('position vs time');
 figure(2)
 plot(t,z(:,2));
+hold on
+plot(t,y);
+legend('state','measurement');
 title('velocity vs time');
--- springprocess.m
+++ springprocess.m
@@ -4,11 +4,7 @@ k = p.k;
 m = p.m;
 omega = p.omega;
 
-
 dx = z(2);
-
-
-
 dv = (-k*z(1) + 10*sin(omega*t))/m;
 
 % dv = (-k*z(1))/m;
